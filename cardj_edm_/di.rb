e75e23553db0cab620dbf59bfb3452d2d7db3c9b
TheSmallBoat <abecai@gmail.com>
2025-09-30 01:14:47 +0800

    feat: Refactor function names and improve code clarity; fix clamp usage and enhance sleep calculations for better performance

--- cardj_edm_/di.rb
+++ cardj_edm_/di.rb
@@ -1,18 +1,16 @@
-# Dawn Ignition 《晨启》
+# Dawn Ignition 《晨启》- 修复版
 # 数学驱动的渐进式House，借鉴经验技法
 
 use_bpm 118
 use_debug false
 
-use_bpm 118
-
-define :pi_sequence do |index, offset = 0|
+define :pi_seq do |index, offset = 0|
   pi_str = "31415926535897932384626433832795028841971693993751"
   digit = pi_str[(index + offset) % pi_str.length].to_i
   digit / 9.0
 end
 
-define :phi_evolution do |time, frequency = 1.0|
+define :pe do |time, frequency = 1.0|
   phi = 1.618033988749895
   (Math.sin(time * frequency * phi) * 0.5 + 0.5)
 end
@@ -26,7 +24,7 @@ define :time_coordinates do |global_time|
   }
 end
 
-define :deephouse_techniques do |energy_level|
+define :dt do |energy_level|
   {
     gentle_kick: {
       pattern: [1, 0, 0.3, 0],
@@ -34,7 +32,7 @@ define :deephouse_techniques do |energy_level|
       condition: energy_level < 0.3
     },
     
-    chord_progression: [chord(:c4, :M7),chord(:f4, :M7),chord(:g4, :dom7),chord(:c4, :M7)],
+    cp: [chord(:c4, :M7),chord(:f4, :M7),chord(:g4, :dom7),chord(:c4, :M7)],
     
     wobble_params: {
       phase: 2.0 + energy_level * 2.0,
@@ -51,8 +49,9 @@ define :deephouse_techniques do |energy_level|
 end
 
 define :dawn_fusion_controller do |ultra_time|
+  # 修复 clamp 用法
   stage_raw = (ultra_time * 5).to_i
-  stage = stage_raw < 0 ? 0 : (stage_raw > 4 ? 4 : stage_raw)
+  stage = [[stage_raw, 0].max, 4].min  # 替代 clamp(0, 4)
   stage_progress = ((ultra_time * 5) % 1.0)
   
   transition = 1.0 / (1.0 + Math.exp(-8 * (stage_progress - 0.5)))
@@ -73,7 +72,7 @@ define :dawn_fusion_controller do |ultra_time|
       synth_primary: :dpulse,
       scale: :major,
       reverb_space: 0.7,
-      borrowed_technique: :chord_progression
+      borrowed_technique: :cp
     },
     
     {
@@ -106,8 +105,8 @@ define :dawn_fusion_controller do |ultra_time|
   
   current_config = base_configs[stage]
   
-  current_config[:energy] += phi_evolution(ultra_time * 20) * 0.1
-  current_config[:micro_variations] = pi_sequence((ultra_time * 1000).to_i)
+  current_config[:energy] += pe(ultra_time * 20) * 0.1
+  current_config[:micro_variations] = pi_seq((ultra_time * 1000).to_i)
   
   [stage, current_config, transition]
 end
@@ -120,7 +119,7 @@ set :dawn_config, {
   synth_primary: :blade,
   scale: :major_pentatonic,
   reverb_space: 0.9,
-  techniques: deephouse_techniques(0.15)
+  techniques: dt(0.15)
 }
 set :dawn_transition, 0.0
 
@@ -130,7 +129,7 @@ live_loop :dawn_master_controller do
   
   coords = time_coordinates(t)
   fusion_data = dawn_fusion_controller(coords[:ultra])
-  techniques = deephouse_techniques(fusion_data[1][:energy])
+  techniques = dt(fusion_data[1][:energy])
   
   set :dawn_stage, fusion_data[0]
   set :dawn_config, fusion_data[1].merge(techniques: techniques)
@@ -150,33 +149,36 @@ live_loop :dawn_kick_fusion, sync: :dawn_master_controller do
   next if config.nil? || config[:techniques].nil?
   
   coords = time_coordinates(t)
-  mathematical_intensity = phi_evolution(coords[:micro] * 4)
+  mathematical_intensity = pe(coords[:micro] * 4)
   borrowed_pattern = config[:techniques][:gentle_kick][:pattern]
   
   should_kick = case config[:kick_style]
   when :gentle_morning
     base_trigger = borrowed_pattern[t % 4] > 0.5
     math_mod = mathematical_intensity > 0.6
-    base_trigger || (math_mod && pi_sequence(t/4) > 0.8)
+    base_trigger || (math_mod && pi_seq(t/4) > 0.8)
     
   when :light_bounce
-    (t % 4 == 0) || (t % 4 == 2 && pi_sequence(t/8) > 0.7)
+    (t % 4 == 0) || (t % 4 == 2 && pi_seq(t/8) > 0.7)
     
   when :steady_four, :house_classic, :full_drive
     complexity = config[:energy]
     base_hits = [0, 2].include?(t % 4)
-    extra_hits = complexity > 0.5 && [1, 3].include?(t % 4) && pi_sequence(t) > (1 - complexity)
+    extra_hits = complexity > 0.5 && [1, 3].include?(t % 4) && pi_seq(t) > (1 - complexity)
     base_hits || extra_hits
   end
   
   if should_kick
     amp_base = config[:techniques][:gentle_kick][:amp_base]
-    amp_variation = pi_sequence(t, 3) * 0.3
+    amp_variation = pi_seq(t, 3) * 0.3
+    
+    # 修复 cutoff 计算，确保在有效范围内
+    cutoff_val = [[60 + config[:energy] * 50, 130].min, 20].max
     
     sample :bd_haus,
       amp: amp_base + amp_variation + config[:energy] * 0.4,
       rate: 0.98 + coords[:micro] * 0.02,
-      cutoff: [60 + config[:energy] * 50, 130].min
+      cutoff: cutoff_val
   end
   
   sleep 0.25
@@ -192,15 +194,15 @@ live_loop :dawn_chord_fusion, sync: :dawn_master_controller do
   
   use_synth config[:synth_primary]
   
-  base_progression = config[:techniques][:chord_progression]
+  base_progression = config[:techniques][:cp]
   chord_index = (coords[:macro] / 4).to_i % base_progression.length
   current_chord = base_progression[chord_index]
   
-  pitch_warp = (pi_sequence(t/32, 7) - 0.5) * 3
+  pitch_warp = (pi_seq(t/32, 7) - 0.5) * 3
   warped_chord = current_chord.map { |note| note + pitch_warp }
   
   wobble_config = config[:techniques][:wobble_params]
-  wobble_phase = wobble_config[:phase] + phi_evolution(coords[:ultra] * 10) * 0.5
+  wobble_phase = wobble_config[:phase] + pe(coords[:ultra] * 10) * 0.5
   
   with_fx :reverb, room: 0.7, mix: config[:reverb_space] do
     with_fx :wobble,
@@ -231,16 +233,16 @@ live_loop :dawn_melody_fusion, sync: :dawn_master_controller do
     use_synth :piano
     
     arpeggio_pattern = config[:techniques][:piano_texture][:arpeggio]
-    current_chord = config[:techniques][:chord_progression][(coords[:macro]/4).to_i % 4]
+    current_chord = config[:techniques][:cp][(coords[:macro]/4).to_i % 4]
     
     pattern_index = (coords[:micro] * 2).to_i % arpeggio_pattern.length
     chord_note_index = arpeggio_pattern[pattern_index]
     base_note = current_chord[chord_note_index % current_chord.length]
     
-    fractal_offset = (pi_sequence(t, 11) - 0.5) * 7
+    fractal_offset = (pi_seq(t, 11) - 0.5) * 7
     final_note = base_note + fractal_offset
     
-    timing_humanize = (pi_sequence(t, 13) - 0.5) * 0.1
+    timing_humanize = (pi_seq(t, 13) - 0.5) * 0.1
     
     with_fx :reverb, room: 0.5, mix: 0.6 do
       with_fx :echo, phase: 0.375, decay: 3, mix: 0.3 do
@@ -251,7 +253,14 @@ live_loop :dawn_melody_fusion, sync: :dawn_master_controller do
       end
     end
     
-    sleep [0.25, 0.5, 0.75][(pi_sequence(t, 17) * 3).to_i] + timing_humanize
+    # 修复 sleep 计算，确保不会是 nil
+    sleep_choices = [0.25, 0.5, 0.75]
+    sleep_index = (pi_seq(t, 17) * 3).to_i % sleep_choices.length
+    base_sleep = sleep_choices[sleep_index]
+    
+    # 确保 timing_humanize 不会让 sleep 变成负数或 nil
+    final_sleep = [base_sleep + timing_humanize, 0.1].max
+    sleep final_sleep
   else
     sleep 2
   end
@@ -267,24 +276,28 @@ live_loop :dawn_ambient_fusion, sync: :dawn_master_controller do
   
   use_synth :hollow
   
-  phi_note_offset = (phi_evolution(coords[:ultra] * 50) * 24).round
+  phi_note_offset = (pe(coords[:ultra] * 50) * 24).round
   ambient_note = :c5 + phi_note_offset
   
-  reverb_room = 0.8 + pi_sequence(t/64) * 0.2
-  reverb_mix = config[:reverb_space] + (pi_sequence(t/32, 5) - 0.5) * 0.2
+  reverb_room = 0.8 + pi_seq(t/64) * 0.2
+  reverb_mix = config[:reverb_space] + (pi_seq(t/32, 5) - 0.5) * 0.2
   
   with_fx :reverb, room: reverb_room, mix: reverb_mix do
-    with_fx :echo, phase: phi_evolution(coords[:ultra] * 30) * 0.5 + 0.25 do
+    with_fx :echo, phase: pe(coords[:ultra] * 30) * 0.5 + 0.25 do
       play ambient_note,
         attack: 3 + config[:energy] * 2,
         release: 12 - config[:energy] * 4,
         amp: 0.1 + (1 - config[:energy]) * 0.2,
-        cutoff: 60 + pi_sequence(t/16) * 30,
-        pan: (pi_sequence(t/8, 9) - 0.5) * 0.8
+        cutoff: 60 + pi_seq(t/16) * 30,
+        pan: (pi_seq(t/8, 9) - 0.5) * 0.8
     end
   end
   
-  sleep 8 + pi_sequence(t/4) * 4
+  # 修复 sleep 计算
+  base_sleep = 8
+  sleep_variation = pi_seq(t/4) * 4
+  final_sleep = [base_sleep + sleep_variation, 1].max
+  sleep final_sleep
 end
 
 live_loop :dawn_bass_fusion, sync: :dawn_master_controller do
@@ -299,7 +312,7 @@ live_loop :dawn_bass_fusion, sync: :dawn_master_controller do
     use_synth :dsaw
     use_synth_defaults release: 0.1, amp: 1.0 + config[:energy] * 0.5
     
-    current_chord = config[:techniques][:chord_progression][(coords[:macro]/4).to_i % 4]
+    current_chord = config[:techniques][:cp][(coords[:macro]/4).to_i % 4]
     root_note = current_chord[0] - 24
     
     bass_pattern = [
@@ -310,7 +323,7 @@ live_loop :dawn_bass_fusion, sync: :dawn_master_controller do
     note_to_play = bass_pattern[t % bass_pattern.length]
     
     if note_to_play != 0
-      if pi_sequence(t, 19) < (config[:energy] * 0.8 + 0.2)
+      if pi_seq(t, 19) < (config[:energy] * 0.8 + 0.2)
         play note_to_play,
           cutoff: 80 + config[:energy] * 20,
           sustain: 0.08
